fbf6b79bd6091f3bb19eeadd719a626127fc0e65
Tito Tamayo <xlord500@gmail.com>
2017-01-10 08:46:39 +0000

    Added hirb gem

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,17 +1,18 @@
 class ContactsController < ApplicationController 
   def new
-    @contact = Contact.new
+    @contact = Contact.new #creates a blank contact object
   end
   
   def create
-    @contact = Contact.new(contact_params)
-    if @contact.save
-      redirect_to new_contact_path, notice: "Message sent."
+    @contact = Contact.new(contact_params) #Fills in the blank parameters set in the contact object
+    if @contact.save #saves the newly filled contact form to the database
+      redirect_to new_contact_path, notice: "Message sent." #save success
     else
-      redirect_to new_contact_path, notice: "Error occured."
+      redirect_to new_contact_path, notice: "Error occured." #save failed
     end
   end
   
+  #Gets the contact forms parameters 
   private
   def contact_params
     params.require(:contact).permit(:name, :email, :comments)
